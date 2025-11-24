d71bb49d7cd6fdaa1566eccb1cefc7c57bb615e2
JuHan Park <iiiamst16@gmail.com>
2025-11-24 13:04:30 +0900

    fix: Router -> RouterProvider로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-import React, {useState} from "react"
-import './App.css'
-import { Routes, Route, Link } from "react-router-dom";
-import { router } from "./router/index.jsx";
+import { RouterProvider } from 'react-router-dom'
+import { router } from '@/router/index.jsx'
 
-
-
-function App() {
-  return (
-
-    <Routes router={router}/>
-  )
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
