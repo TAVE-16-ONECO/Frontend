32d8eeac0d8712cc64f2a2d6ba12dd2b04b70a33
JuHan Park <iiiamst16@gmail.com>
2026-01-12 12:30:05 +0900

    fix: 헤더 및 로그인 화면 UI 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 const Header = () => {
   return (
     <div className='h-[40px] flex items-end'>
-      <p className='ms-[20px] text-[28px] font-header'>ONECO</p>
+      <p className='ms-[20px]'>
+        <img
+          className='w-[100px]'
+          src='/images/OnecoLogo.png'
+          alt='로고 이미지'
+        />
+      </p>
     </div>
   )
 }
