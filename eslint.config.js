fe903e9c32856137c3ab6efa215d34ad194f0819
JuHan Park <iiiamst16@gmail.com>
2026-01-15 18:07:21 +0900

    fix: eslint 설정이 제대로 동작하지 않던 오류 수정

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import pluginQuery from '@tanstack/eslint-plugin-query'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
@@ -10,14 +9,26 @@ import plugin from 'eslint-plugin-react'
 
 export default defineConfig([
   globalIgnores(['dist', 'dev-dist']),
+  // 기본 JS 권장 설정
+  js.configs.recommended,
+
+  // React 관련 설정
+  plugin.configs.flat['jsx-runtime'],
+
+  // React Hooks 및 Refresh 설정
+  {
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
+
+  // 프로젝트 전역 설정 및 커스텀 규칙
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      plugin.configs.flat['jsx-runtime'],
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -31,6 +42,8 @@ export default defineConfig([
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
   },
+
+  // 기타 플러그인
   ...pluginQuery.configs['flat/recommended'],
   eslintConfigPrettier
 ])
